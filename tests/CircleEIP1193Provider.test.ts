d11aec3de35c6b94a09c39a12738ca8368493530
Antony Fleischer <antony.fleischer@circle.com>
2024-07-18 12:06:13 -0700

    Update tests

--- tests/CircleEIP1193Provider.test.ts
+++ tests/CircleEIP1193Provider.test.ts
@@ -1,29 +1,26 @@
 import { describe, beforeAll, test, expect } from '@jest/globals';
 import CircleEIP1193Provider from '../src/CircleEIP1193Provider';
 import { config } from '../src/config';
+import { setupCircleProvider } from '../src/setupCircleProvider';
 
 describe('CircleEIP1193Provider', () => {
   let provider: CircleEIP1193Provider;
 
   beforeAll(async () => {
-    provider = await CircleEIP1193Provider.create(config.apiKey);
+    provider = await setupCircleProvider(config.apiKey);
   });
 
-  test('publicKey should be a non-null string', () => {
-    return provider.fetchPublicKey().then(publicKey => {
-      console.log('Fetched Public Key:', publicKey);
-      expect(publicKey).not.toBeNull();
-      expect(typeof publicKey).toBe('string');
-    });
-
+  test('publicKey should be a non-null string', async () => {
+    const publicKey = await provider.fetchPublicKey();
+    console.log('Fetched Public Key:', publicKey);
+    expect(publicKey).not.toBeNull();
+    expect(typeof publicKey).toBe('string');
   });
 
-  test('wallet set should be non-null UUID string', () => {
-    return provider.createWalletSet().then(walletSetId => {
-      console.log('Created wallet set:', walletSetId);
-      expect(walletSetId).not.toBeNull();
-      expect(typeof walletSetId).toBe('string');
-    });
-
+  test('wallet set should be non-null UUID string', async () => {
+    const walletSetId = await provider.createWalletSet();
+    console.log('Created wallet set:', walletSetId);
+    expect(walletSetId).not.toBeNull();
+    expect(typeof walletSetId).toBe('string');
   });
-})
+});
